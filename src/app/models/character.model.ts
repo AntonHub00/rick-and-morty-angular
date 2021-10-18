fb5e2c09ae95fc1394bb8eff6a6ce00db1af32dc
Antonio Ochoa <antonhub00@gmail.com>
2021-10-17 21:18:35 -0500

    Field added to character model

--- src/app/models/character.model.ts
+++ src/app/models/character.model.ts
@@ -1,5 +1,11 @@
 export class Character {
+  id: number | undefined;
   name: string | undefined;
   status: string | undefined;
+  species: string | undefined;
+  type: string | undefined;
+  gender: string | undefined;
   image: string | undefined;
+  origin: { name: string } | undefined;
+  location: { name: string } | undefined;
 }
