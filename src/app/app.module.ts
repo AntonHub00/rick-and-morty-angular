b60b14d481976eff4296739c8e4d9612f363133f
Antonio Ochoa <antonhub00@gmail.com>
2021-10-17 21:36:38 -0500

    Character details view component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './views/main/main.component';
 import { PreviewCardComponent } from './components/preview-card/preview-card.component';
 import { DetailsCardComponent } from './components/details-card/details-card.component';
+import { CharacterDetailsComponent } from './views/character-details/character-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     PreviewCardComponent,
-    DetailsCardComponent
+    DetailsCardComponent,
+    CharacterDetailsComponent
   ],
   imports: [
     BrowserModule
