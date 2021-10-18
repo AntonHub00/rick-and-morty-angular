d3040b3acb4de376729a78b1b7ba3189f00c45e4
Antonio Ochoa <antonhub00@gmail.com>
2021-10-17 19:25:40 -0500

    First preview card implemetation added

--- src/app/components/preview-card/preview-card.component.ts
+++ src/app/components/preview-card/preview-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Character } from 'src/app/models/character.model';
 
 @Component({
   selector: 'app-preview-card',
   templateUrl: './preview-card.component.html',
-  styleUrls: ['./preview-card.component.scss']
+  styleUrls: ['./preview-card.component.scss'],
 })
 export class PreviewCardComponent implements OnInit {
-
-  constructor() { }
+  @Input('character') character: Character = new Character();
 
   ngOnInit(): void {
+    this.character.name = 'Test name';
+    this.character.status = 'Test status';
+    this.character.image = 'https://via.placeholder.com/300';
   }
-
 }
---
+++ src/app/models/character.model.ts
@@ -0,0 +1,5 @@
+export class Character {
+  name: string | undefined;
+  status: string | undefined;
+  image: string | undefined;
+}
