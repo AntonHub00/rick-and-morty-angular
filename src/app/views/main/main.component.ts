da20ad90aefff871b4c6925c8240d3dc74592b57
Antonio Ochoa <antonhub00@gmail.com>
2021-10-18 04:41:37 -0500

    Main view working

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ICharacter } from 'src/app/models/ICharacter.model';
+import { RickAndMortyAPIService } from 'src/app/services/rickAndMortyAPI/rick-and-morty-api.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  characters: ICharacter[] = [];
 
-  constructor() { }
+  constructor(private rickAndMortyAPIService: RickAndMortyAPIService) {}
 
   ngOnInit(): void {
+    this.rickAndMortyAPIService.getAllCharacters().subscribe(
+      (response) => (this.characters = response),
+      (error) => {
+        console.log("ERROR: Couldn't get all characters");
+        console.log(error);
+      }
+    );
   }
-
 }
